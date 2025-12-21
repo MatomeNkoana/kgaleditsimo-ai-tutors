9597d40786f7344902529b8100cd8be4c4972e7a
Matome Nkoana <m.k.nkoana17@gmail.com>
2025-12-21 22:42:22 +0200

    feat(client): implement chat wiring to send and receive messages

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify , request
 from flask_cors import CORS
 
 
@@ -12,7 +12,14 @@ curriculum_data = {
             "modules": [
                 { "title": "Organic Chemistry", "id": "chem-org" },
                 { "title": "Inorganic Chemistry", "id": "chem-inorg" },
-                { "title": "Analytical Chemistry", "id": "chem-ana" },
+                { 
+                    "title": "Analytical Chemistry", 
+                    "id": "chem-ana",
+                    "topics": [
+                        { "title": "Significant Figures", "content": "Non-zero digits are always significant." },
+                        { "title": "Titration", "content": "Determining concentration using a known solution." }
+                    ]
+                },
                 { "title": "Physical Chemistry", "id": "chem-phys" }
             ]
         },
@@ -41,12 +48,27 @@ CORS(app)
 @app.route('/')
 def home():
     return "System Online: Kgaleditsimo AI Backend is running."
-
+# When someone visits the root URL ('/api/curriculum'), run this function. That loads the data
 @app.route('/api/curriculum')
 def get_curriculum():
     #jsonify converts the Python dictionary into a JSON response
     return jsonify(curriculum_data)
+# Chat Endpoint (POST)
+# 'methods=['POST']' means this route only accepts data submission, not casual browsing.
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    # A. Get the data sent by the frontend
+    user_data = request.json
+    user_question = user_data.get('question')
 
+    # B. (Placeholder) AI Logic goes here
+    # For now, we will just simulate intelligence.
+    print(f"User asked: {user_question}") # Print to your terminal
+    
+    mock_answer = f"I received your question: '{user_question}'. This is a mock AI response."
+
+    # C. Send the answer back
+    return jsonify({"answer": mock_answer})
 # 4. Start the Server
 # debug=True allows the server to auto-restart when you save changes.
 if __name__ == '__main__':
